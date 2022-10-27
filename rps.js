bd45cacb01ee01fb26a30f32c9f41a1af73701be
tyrone-code <68476868+tyrone-code@users.noreply.github.com>
2022-10-27 11:47:00 +0200

    added code from codepen to generate random int when button is pressed

--- rps.js
+++ rps.js
@@ -1,3 +1,13 @@
-function helloWorld(){
-    alert("hello World")
+const message = 'Hello world' // Try edit me
+
+// Update header text
+//document.querySelector('#header').innerHTML = message
+function randInt(){
+  var randNum = Math.floor(Math.random() * 100);
+  document.querySelector('#header').innerHTML = randNum
 }
+if (randNum > 50){
+  console.log("oui")
+}
+// Log to console
+console.log(message)
