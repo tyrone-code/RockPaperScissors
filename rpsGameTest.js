6b0d43caaa08387760e064c476ac80407a6ed94c
tyrone-code <68476868+tyrone-code@users.noreply.github.com>
2022-11-01 19:21:35 +0200

    added working buttons each corrisponding to rock paper and scissors

--- rpsGameTest.js
+++ rpsGameTest.js
@@ -1,28 +1,31 @@
 
 function rockPaperScissors(){
-    let user = prompt("Enter either Rock paper or scissors");
+    //let user = prompt("Enter either Rock paper or scissors");
     let computer = Math.floor((Math.random() * 3) + 1);
     let paper = (computer === 1); // 
     let scissors = (computer === 2); // 
     let rock = (computer === 3);
+    let buttonRock = document.getElementById("rock");
+    let buttonPaper = document.getElementById("paper");
+    let buttonScissors = document.getElementById("scissors");
    
     // rock is 1
     // paper is 2
     // scissors is 3
-
-if (user === "rock" && paper === true){
+   
+if (buttonRock && paper === true){
     alert("computer chose paper you loose!");
     console.log(computer);
     console.log(paper);   
 
     }
-else if (user === "rock" && rock === true){
+else if (buttonRock && rock === true){
     alert("computer chose rock aswell it is a draw!");
     console.log(computer);
     console.log(rock);   
 }
 
-else if (user === "rock" && paper === false){
+else if (buttonRock && scissors === true){
     alert("computer chose scissors you win!");
     console.log(computer);
      
@@ -30,13 +33,13 @@ else if (user === "rock" && paper === false){
 }
 
 //_------------------------------------------------------- paper
-else if (user === "paper" && scissors === true){
+else if (buttonPaper && scissors === true){
     alert("computer chose scissors you lose!");
     console.log(computer);
 
 }
 
-else if (user === "paper" && paper === true){
+else if (buttonPaper && paper === true){
     alert("computer chose Paper aswell! it is a draw");
     console.log(computer);
      
@@ -44,7 +47,7 @@ else if (user === "paper" && paper === true){
 }
 
 
-else if (user === "paper" && rock === true){
+else if (buttonPaper && rock === true){
     alert("computer chose rock you win!");
     console.log(computer);
 }
@@ -74,4 +77,121 @@ else if (user === "scissors" && paper === true){
 }
 
 
-rockPaperScissors();
+//rockPaperScissors();
+
+
+function Rock(){
+      //let user = prompt("Enter either Rock paper or scissors");
+    let computer = Math.floor((Math.random() * 3) + 1);
+    let paper = (computer === 1); // 
+    let scissors = (computer === 2); // 
+    let rock = (computer === 3);
+    let buttonRock = document.getElementById("rock");
+
+    if (buttonRock && paper === true){
+        alert("computer chose paper you loose!");
+        console.log(computer);
+        console.log(paper);   
+    
+        }
+    else if (buttonRock && rock === true){
+        alert("computer chose rock aswell it is a draw!");
+        console.log(computer);
+        console.log(rock);   
+    }
+    
+    else if (buttonRock && scissors === true){
+        alert("computer chose scissors you win!");
+        console.log(computer);
+         
+    
+    }
+}
+
+
+function Paper(){
+    //let user = prompt("Enter either Rock paper or scissors");
+  let computer = Math.floor((Math.random() * 3) + 1);
+  let paper = (computer === 1); // 
+  let scissors = (computer === 2); // 
+  let rock = (computer === 3);
+  let buttonPaper = document.getElementById("paper");
+  
+  if (buttonPaper && paper === true){
+      alert("computer chose paper its a a draw");
+      console.log(computer);
+      console.log(paper);   
+  
+      }
+  else if (buttonPaper && rock === true){
+      alert("computer chose rock you win!");
+      console.log(computer);
+      console.log(rock);   
+  }
+  
+  else if (buttonPaper && scissors === true){
+      alert("computer chose scissors you lose!");
+      console.log(computer);
+       
+  
+  }
+}
+
+
+
+function Rock(){
+    //let user = prompt("Enter either Rock paper or scissors");
+  let computer = Math.floor((Math.random() * 3) + 1);
+  let paper = (computer === 1); // 
+  let scissors = (computer === 2); // 
+  let rock = (computer === 3);
+  let buttonRock = document.getElementById("rock");
+
+  if (buttonRock && paper === true){
+      alert("computer chose paper you loose!");
+      console.log(computer);
+      console.log(paper);   
+  
+      }
+  else if (buttonRock && rock === true){
+      alert("computer chose rock aswell it is a draw!");
+      console.log(computer);
+      console.log(rock);   
+  }
+  
+  else if (buttonRock && scissors === true){
+      alert("computer chose scissors you win!");
+      console.log(computer);
+       
+  
+  }
+}
+
+
+function Scissor(){
+  //let user = prompt("Enter either Rock paper or scissors");
+let computer = Math.floor((Math.random() * 3) + 1);
+let paper = (computer === 1); // 
+let scissors = (computer === 2); // 
+let rock = (computer === 3);
+let buttonScissors = document.getElementById("scissor");
+
+if (buttonScissors && paper === true){
+    alert("computer chose paper you win!");
+    console.log(computer);
+    console.log(paper);   
+
+    }
+else if (buttonScissors && rock === true){
+    alert("computer chose rock you lose!");
+    console.log(computer);
+    console.log(rock);   
+}
+
+else if (buttonScissors && scissors === true){
+    alert("computer chose scissors its a draw! klink!");
+    console.log(computer);
+     
+
+}
+}
