fbe6c7c00ad38d0808def9dac60483d12000a51f
tyrone-code <68476868+tyrone-code@users.noreply.github.com>
2022-11-01 17:13:52 +0200

    finished basic logic behind rock paper scissors in Javascript

--- rpsGameTest.js
+++ rpsGameTest.js
@@ -2,80 +2,76 @@
 function rockPaperScissors(){
     let user = prompt("Enter either Rock paper or scissors");
     let computer = Math.floor((Math.random() * 3) + 1);
-    let paper = (computer === 1); // paper is greater than equal to 10
-    let scissors = (computer === 2); // scissors is less then 10 but not greater then 5
+    let paper = (computer === 1); // 
+    let scissors = (computer === 2); // 
     let rock = (computer === 3);
    
     // rock is 1
     // paper is 2
     // scissors is 3
-// if paper is greater then the value 10 paper wins else not
+
 if (user === "rock" && paper === true){
     alert("computer chose paper you loose!");
     console.log(computer);
-    console.log(paper);
-    
+    console.log(paper);   
 
+    }
+else if (user === "rock" && rock === true){
+    alert("computer chose rock aswell it is a draw!");
+    console.log(computer);
+    console.log(rock);   
 }
 
 else if (user === "rock" && paper === false){
-    alert("computer choose scissors you win!");
+    alert("computer chose scissors you win!");
     console.log(computer);
-    console.log(paper);
+     
+
 }
 
+//_------------------------------------------------------- paper
 else if (user === "paper" && scissors === true){
-  alert("computer choose scissors you lose!");
+    alert("computer chose scissors you lose!");
     console.log(computer);
-    console.log(scissors);
+
 }
-    
-else if (user === "paper" && scissors === false){
-       alert("computer choose rock you win!");
-        console.log(computer);
-        console.log(rock);
-    }
+
+else if (user === "paper" && paper === true){
+    alert("computer chose Paper aswell! it is a draw");
+    console.log(computer);
+     
+
+}
 
 
+else if (user === "paper" && rock === true){
+    alert("computer chose rock you win!");
+    console.log(computer);
+}
+//_------------------------------------------------------- scissors
+else if (user === "scissors" && scissors === true){
+    alert("computer chose scissors its a draw KLINK!");
+    console.log(computer);
+
+}
 
 else if (user === "scissors" && rock === true){
-   alert("computer choose rock you lose");
+    alert("computer chose Rock you lose!");
     console.log(computer);
-    console.log(rock);
-    }
+     
+
+}
 
 
-else if (user === "scissors" && rock === false){
-    alert("computer choose paper you win!");
+else if (user === "scissors" && paper === true){
+    alert("computer chose Paper you win!");
     console.log(computer);
-    console.log(scissors);
-        }
+     
 
-
-    
-    
-
-
-
+}
 
 
 }
 
 
-
-
-rockPaperScissors()
-/*else if (user === "paper"){
-    console.log("you enter paper");
-    console.log(computer);
-}
-
-else if (user === "scissors"){
-    console.log("you entered scissors");
-    console.log(computer);
-}
-
-else{
-    console.log("please enter a valid value");
-    console.log(computer)
-*/
+rockPaperScissors();
