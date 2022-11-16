538e85c2dd1105cec88f6d9217faae773f77a6db
tyrone-code <68476868+tyrone-code@users.noreply.github.com>
2022-11-16 16:28:08 +0200

    made a point system if a user reaches the victory point a alert box will appear as you win or else as you lose

--- rpsGameTest.js
+++ rpsGameTest.js
@@ -2,7 +2,8 @@
 var computerPoints = 0
 var userPoints = 0
 
-     
+
+
 function Rock(){
       //let user = prompt("Enter either Rock paper or scissors");
     let computer = Math.floor((Math.random() * 3) + 1);
@@ -12,9 +13,22 @@ function Rock(){
     let buttonRock = document.getElementById("rock");
     document.getElementById("paper").style.display = "none";
     document.getElementById("scissors").style.display = "none";
+    let endResult = 5
+    if (computerPoints === endResult){
+      alert("You lose!")
+      document.getElementById("computerPoints").innerHTML = 0
+      document.getElementById("userPoints").innerHTML = 0
+      window.location.reload(true)
 
+    }
+    else if (userPoints === endResult){
+      alert("You win!")
+      document.getElementById("computerPoints").innerHTML = 0
+      document.getElementById("userPoints").innerHTML = 0
+      window.location.reload(true)
+    }
    
-     if (buttonRock && paper === true){
+     else if (buttonRock && paper === true){
         console.log(computer);
         console.log(paper);  
         computerPoints = computerPoints + 1
@@ -46,8 +60,12 @@ function Rock(){
         document.getElementById("winMessage").innerHTML = "Computer chose scissors you win!";
         document.getElementById("winMessage").style.display = "block"
     }
+ 
 
 }
+
+// 
+
 
 function Paper(){
     //let user = prompt("Enter either Rock paper or scissors");
@@ -56,11 +74,24 @@ function Paper(){
   let scissors = (computer === 2); // 
   let rock = (computer === 3);
   let buttonPaper = document.getElementById("paper");
+  let endResult = 5
   document.getElementById("rock").style.display = "none";
   document.getElementById("scissors").style.display = "none";
+  if (computerPoints === endResult){
+    alert("You lose!")
+    document.getElementById("computerPoints").innerHTML = 0
+    document.getElementById("userPoints").innerHTML = 0
+    window.location.reload(true)
 
+  }
+  else if (userPoints === endResult){
+    alert("You win!")
+    document.getElementById("computerPoints").innerHTML = 0
+    document.getElementById("userPoints").innerHTML = 0
+    window.location.reload(true)
+  }
   
-  if (buttonPaper && paper === true){
+  else if (buttonPaper && paper === true){
       console.log(computer);
       console.log(paper);   
       document.getElementById("paperImg").style.display = "block";
@@ -95,17 +126,33 @@ function Paper(){
   }
 }
 
+
+
 function Scissors(){
   //let user = prompt("Enter either Rock paper or scissors");
 let computer = Math.floor((Math.random() * 3) + 1);
 let paper = (computer === 1); // 
 let scissors = (computer === 2); // 
 let rock = (computer === 3);
+let endResult = 5
 let buttonScissors = document.getElementById("scissors");
 document.getElementById("rock").style.display = "none";
 document.getElementById("paper").style.display = "none";
+if (computerPoints === endResult){
+  alert("You lose!")
+  document.getElementById("computerPoints").innerHTML = 0
+  document.getElementById("userPoints").innerHTML = 0
+  window.location.reload(true)
 
-if (buttonScissors && paper === true){
+}
+else if (userPoints === endResult){
+  alert("You win!")
+  document.getElementById("computerPoints").innerHTML = 0
+  document.getElementById("userPoints").innerHTML = 0
+  window.location.reload(true)
+}
+
+else if (buttonScissors && paper === true){
     console.log(computer);
     console.log(paper);
     userPoints = userPoints + 1
@@ -141,6 +188,8 @@ else if (buttonScissors && scissors === true){
 }
 }
 
+
+
 function showImagesRock() {
     document.getElementById("paper").style.display = "block";
     document.getElementById("scissors").style.display = "block";
@@ -171,6 +220,8 @@ document.getElementById("loseMessage").style.display = "none"
 document.getElementById("drawMessage").style.display = "none"
 document.getElementById("winMessage").style.display = "none"
   }
+
+
   function showImagesPaper() {
     document.getElementById("rock").style.display = "block";
     document.getElementById("scissors").style.display = "block";
@@ -192,6 +243,9 @@ document.getElementById("winMessage").style.display = "none"
    
   }
 
+
+
+
   function showImagesScissors() {
     document.getElementById("paper").style.display = "block";
     document.getElementById("rock").style.display = "block";
@@ -212,27 +266,27 @@ document.getElementById("winMessage").style.display = "none"
   }
 
 // Get the modal
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
